25dc2adf3d16302d852921c0f684c0d8f69de939
Bibiwei <bibiweipere@gmail.com>
2023-11-29 08:42:32 +0100

    Tasks

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for hexa in range(0, 99):
+        print("{:d} = 0x{:c}".format(hexa, hexa))
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for numbers in range(0, 100):
+        print("{:0>2d}, ".format(numbers), end=" ")
