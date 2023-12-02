5639a380a9b363f7474817068d15e521f939f088
Bibiwei <bibiweipere@gmail.com>
2023-12-02 09:54:42 +0100

    Updates

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -11,11 +11,3 @@ def new_in_list(my_list, idx, element):
         elif idx == index:
             new_list[index] = element
             return my_list[::-1]
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
