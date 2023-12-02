36588260621f5103098b58e54c40e4156c06a680
Bibiwei <bibiweipere@gmail.com>
2023-12-02 23:24:48 +0100

    Updates

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+
+    cpy_str = ""
+    for i in my_string:
+        if i == 'C':
+            cpy_str += ""
+        else:
+            cpy_str += i
+    return cpy_str
