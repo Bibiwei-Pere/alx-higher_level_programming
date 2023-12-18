b7699fc516f09628f34bed2449c9ff7ebc6dc5a0
Bibiwei <bibiweipere@gmail.com>
2023-12-18 21:13:15 +0100

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,8 +11,3 @@ def safe_print_list_integers(my_list=[], x=0):
         len = len + 1
     print("")
     return len
-    
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
