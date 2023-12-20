d97bb4e9ac831b9e9aa460aef4b282d25bd35d1f
Bibiwei <bibiweipere@gmail.com>
2023-12-20 21:17:37 +0100

    Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,6 +13,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+           
     def area(self):
         return self.__size**2
