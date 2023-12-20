46054444874b938b8cde595ce8753d49a9ef8d24
Bibiwei <bibiweipere@gmail.com>
2023-12-20 21:16:59 +0100

    Square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """
-	Module 2-square
-	Square class now has an if statement
+Module 2-square
+Square class now has an if statement
 """
 
 
 class Square:
-    """This defines a private Square attribute"""
     def __init__(self, size=0):
-        """If statement"""
         if type(size) != int:
-            """Raise an error"""
             raise TypeError("size must be an integer")
         elif size < 0:
-            """Raise an error"""
             raise ValueError("size must be >= 0")
         else:
-            """initialize __size of self with size"""
             self.__size = size
             
     def area(self):
-        """Area of a square
-        Returns:
-        	area
-         """
         return self.__size**2
